16762e3351af43cc5a052c2a5f774a8946dc6410
r.ageev <r.ageev@tinkoff.ru>
2022-09-05 16:27:39 +0300

    Added working parser for long_rent in MSC and MSC region

--- cianparser/rentsparser.py
+++ cianparser/rentsparser.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 import transliterate
 import re
 import pymorphy2
-
+from re import findall
 from cianparser.constants import *
 
 
 class ParserRentOffers:
     def __init__(self, type_offer: str, type_accommodation: str, location_id: str, rooms, start_page: int, end_page: int):
         self.session = requests.Session()
-        self.session.headers = {'Accept-Language': 'ru'}
+        self.session.headers = {'Accept-Language': 'ru', "Accept": "text/html"}
 
         self.result = []
         self.type_accommodation = type_accommodation
@@ -50,31 +50,34 @@ class ParserRentOffers:
 
     def load_page(self, number_page=1):
         self.url = self.build_url().format(number_page, self.location_id)
+
         res = self.session.get(url=self.url)
+
+
         res.raise_for_status()
         return res.text
 
     def parse_page(self, html: str, number_page: int):
         try:
             soup = BeautifulSoup(html, 'lxml')
+            with open('soup.txt', 'w') as file:
+                file.write(soup.prettify())
         except:
             soup = BeautifulSoup(html, 'html.parser')
 
         offers = soup.select("div[data-name='HeaderDefault']")
         title = offers[0].text
-        city = title[:title.find('Аренда')].split()[-1]
+
         morph = pymorphy2.MorphAnalyzer()
-        city = morph.parse(city)[0].normal_form.title()
 
-        offers = soup.select("div[data-name='Offers'] > article[data-name='CardComponent']")
-
+        offers = soup.select('div[class="_93444fe79c--wrapper--W0WqH"] > article[data-name="CardComponent"]')
         if number_page == self.start_page:
-            print("Setting [", end="")
+            print(f"Setting {len(offers)} offers [", end="")
             print("=>"*len(offers), end="")
             print("] 100%")
 
         print(f"{number_page} page: ", end="")
-        print("[", end="")
+        print("[ ", end="")
         for block in offers:
             self.parse_block(block=block)
         print("] 100%")
@@ -85,84 +88,127 @@ class ParserRentOffers:
             text_offer = soup_offer_page.select("div[data-name='BtiContainer'] > div[data-name='BtiHouseData']")[0].text
             year = int(text_offer[text_offer.find("Год постройки")+13: text_offer.find("Год постройки") + 17])
         except:
+            print("LOG: Year couldn't be retrieved")
             year = -1
+            year_soup = soup_offer_page.select("div[data-testid='object-summary-description-info'] > div[data-testid='object-summary-description-value']")
+            for var in year_soup:
+                f = findall(r"\b\d\d\d\d\b", var.text)
+                if len(f) == 1:
+                    year = f[0]
 
         try:
             text_offer = soup_offer_page.select("div[data-name='ObjectSummaryDescription'] > div > div:nth-child(1)")[0].text
             comm = (text_offer[: text_offer.find("Общая")])
-            comm_meters = int(re.findall(r'\d+', comm)[0])
+            comm_meters = int(findall(r'\d+', comm)[0])
         except IndexError:
             text_offer = soup_offer_page.select("div[data-name='ObjectSummaryDescription'] > div")[0].text
             comm = (text_offer[: text_offer.find("Общая")])
-            comm_meters = int(re.findall(r'\d+', comm)[0])
+            comm_meters = int(findall(r'\d+', comm)[0])
         except:
             comm_meters = -1
 
+        overall_floors = -1
+        exact_floor = -1
         try:
-            text_offer = soup_offer_page.select("div[data-name='ObjectSummaryDescription'] > div > div:nth-child(3)")[0].text
-            kitchen = (text_offer[text_offer.find("Кухня")-6: text_offer.find("Кухня")])
-            kitchen_meters = int(re.findall(r'\d+', kitchen)[0])
+            text_offer = soup_offer_page.select("div[data-testid='object-summary-description-info'] > div[data-testid='object-summary-description-value']")
+
+            for value in text_offer:
+                f = findall(r'\d+', value.text)
+                if len(f) == 2:
+                    exact_floor = f[0]
+                    overall_floors = f[1]
+        except:
+            print("LOG: overall_floors and exact_floor couldn't be retrieved")
+
+        kitchen_meters = -1
+        try:
+            text_offer = soup_offer_page.select("div[data-testid='object-summary-description-info-block'] > div[data-testid='object-summary-description-info']")
+            for info in text_offer:
+                if info.select("div[data-testid='object-summary-description-title']")[0].text == "Кухня":
+                    kitchen_meters = int(findall(r"\d+", info.select("div[data-testid='object-summary-description-value']")[0].text)[0])
         except IndexError:
             text_offer = soup_offer_page.select("div[data-name='ObjectSummaryDescription'] > div")[0].text
             if "Кухня" in text_offer:
                 kitchen = (text_offer[text_offer.find("Кухня")-6: text_offer.find("Кухня")])
-                kitchen_meters = int(re.findall(r'\d+', kitchen)[0])
+                kitchen_meters = int(findall(r'\d+', kitchen)[0])
             else:
                 kitchen_meters = -1
         except:
             kitchen_meters = -1
 
-        return (year, comm_meters, kitchen_meters)
+        return (year, comm_meters, kitchen_meters, exact_floor, overall_floors)
 
     def parse_block(self, block):
-        title = block.select("div[data-name='LinkArea']")[0].select("div[data-name='TitleComponent']")[0].text
 
+        title = block.select('div[data-name="LinkArea"]')[0].select("span[data-mark='OfferTitle']")[0].text
+        docs_checked = False
+        print(title)
         try:
-            author = block.select("div[data-name='Agent']")[0].select("div[data-name='ContentRow']")[0].text
+            author = block.select("div[class='_93444fe79c--main-info--Nib9U']")[0].select("a")[0].get("href")
+            author += " " + block.select("div[class='_93444fe79c--main-info--Nib9U']")[0].select("a")[0].text
+
+            author_type = block.select("div[class='_93444fe79c--container--GyJAp'] > span[style='letter-spacing:1px']")[0].text
+
         except:
             try:
-                author = block.select("div[data-name='Agent']")[0].select("a[data-name='AgentTitle']")[0].text
-            except:
-                author = block.select("div[data-name='Agent']")[0].select("span[data-name='AgentTitle']")[0].text
+                print("LOG: Author couldn't be retrieved 1")
+                author = block.select('div[class="_93444fe79c--main-info--Nib9U"]')[0].select(
+                    "div[class='_93444fe79c--name-container--enElO']")[0].text
+                author_type = block.select("div[class='_93444fe79c--container--GyJAp'] > span[style='letter-spacing:1px']")[0].text
 
-        if "Опыт" in author:
-            author = author[:author.find("Опыт")]
+            except:
+                print("LOG: Author couldn't be retrieved 2")
+                author = block.select("div[data-name='Agent']")[0].select("span[data-name='AgentTitle']")[0].text
+                author_type = block.select("div[class='_93444fe79c--container--GyJAp' > span[style='letter-spacing:1px']")[0].text
+
+        try:
+            docs_checked = block.select(
+                "div[data-name='ContentRow'] > div[data-name='Proof']")[0].text
+        except:
+            print("LOG: Docs couldn't be checked")
 
         link = block.select("div[data-name='LinkArea']")[0].select("a")[0].get('href')
 
+        subtitle = ''
         try:
             meters = int(title[title.find("м²") - 4: title.find("м²")])
+
         except:
-            meters = int(title[title.find("м²") - 5: title.find("м²")].split(',')[0])
+            try:
+                print("LOG: Meters couldn't be retrieved 1")
+                meters = int(title[title.find("м²") - 5: title.find("м²")].split(',')[0])
 
-        if "этаж" in title:
-            floor_per = title[title.find("м²") + 3: title.find("м²") + 9]
-            floor_per = floor_per.replace(' ', '')
-            floor_per = floor_per.replace('э', '')
-            floor_per = floor_per.split("/")
-            all_floors = int(floor_per[1])
-            floor = int(floor_per[0])
-        else:
-            all_floors = -1
-            floor = -1
+            except:
+                print("LOG: Meters couldn't be retrieved 2")
+                subtitle = block.select('div[data-name="LinkArea"]')[0].select("div[class='_93444fe79c--subtitle--vHiOV']")[0].text
 
-        if "1-комн" in title or "Студия" in title:
+                m = findall(r"[-+]?(?:\d*,\d+|\d+)", subtitle)[1]
+                meters = ''
+                if "," in m:
+                    m = m.replace(",", ".")
+                if "." in m:
+                    meters = float(m)
+                else:
+                    meters = int(m)
+                print("Meters: " + str(meters))
+
+
+        if "1-комн" in title or "Студия" in title or "1-комн" in subtitle or "Студия" in subtitle:
             how_many_rooms = 1
-        elif "2-комн" in title:
+        elif "2-комн" in title or "2-комн" in subtitle:
             how_many_rooms = 2
-        elif "3-комн" in title:
+        elif "3-комн" in title or "3-комн" in subtitle:
             how_many_rooms = 3
-        elif "4-комн" in title:
+        elif "4-комн" in title or "4-комн" in subtitle:
             how_many_rooms = 4
         else:
             how_many_rooms = -1
 
-        address_long = block.select("div[data-name='LinkArea']")[0].select("div[data-name='ContentRow']")[0].text
-        district = address_long[address_long.find("р-н") + 4:].split(",")[0]
-        street = address_long.split(",")[-2]
-        street = street.replace("улица", "")
+        address_long = block.select("div[data-name='LinkArea']")[0].select("div[class='_93444fe79c--labels--L8WyJ']")[0].text
+        print("Long address")
+        print(address_long)
 
-        price_long = block.select("div[data-name='LinkArea']")[0].select("div[data-name='ContentRow']")[1].text
+        price_long = block.select("div[data-name='LinkArea']")[0].select("span[data-mark='MainPrice']")[0].text
         price_per_month = "".join(price_long[:price_long.find("₽/мес") - 1].split())
         price_per_month = int(price_per_month)
 
@@ -171,36 +217,40 @@ class ParserRentOffers:
         else:
             commissions = 0
 
-        district = transliterate.translit(district, reversed=True)
-        street = transliterate.translit(street, reversed=True)
 
         try:
             author = transliterate.translit(author, reversed=True)
         except:
             pass
-
+        self.session = requests.Session()
+        self.session.headers = {'Accept-Language': 'ru', "Accept": "text/html"}
         res = self.session.get(url=link)
         res.raise_for_status()
         html_offer_page = res.text
 
-        year_of_construction, comm_meters, kitchen_meters = self.parse_page_offer(html_offer=html_offer_page)
+        year_of_construction, comm_meters, kitchen_meters, exact_floor, overall_floors = self.parse_page_offer(html_offer=html_offer_page)
         print("=>", end="")
-        self.result.append({
+        result = {
             "accommodation": self.type_accommodation,
             "how_many_rooms": how_many_rooms,
             "price_per_month": price_per_month,
-            "street": street,
-            "district": district,
-            "floor": floor,
-            "all_floors": all_floors,
+            # "street": street,
+            # "district": district,
+            "address": address_long,
+            "floor": exact_floor,
+            "overall_floors": overall_floors,
             "square_meters": meters,
             "commissions": commissions,
             "author": author,
+            "author_type": author_type,
+            "docs_checked": docs_checked,
             "year_of_construction": year_of_construction,
             "comm_meters": comm_meters,
             "kitchen_meters": kitchen_meters,
             "link": link
-        })
+        }
+        self.result.append(result)
+
 
     def get_results(self):
         return self.result
@@ -214,5 +264,6 @@ class ParserRentOffers:
                 self.parse_page(html=html, number_page=number_page)
             except Exception as e:
                 print(e)
-                print(f"Dont exist this {number_page} page.. Ending parse\n")
+                print(e.with_traceback())
+                print(f"This page number {number_page} doesn't exist... Ending parse\n")
                 break
